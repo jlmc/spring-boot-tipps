2f95eb0e58f27fd25cc4083293c3db16ecf40b99
Joao Costa <costajlmpp@gmail.com>
2019-12-03 19:57:27 +0000

    Initial commit

---
+++ examples-api/src/test/java/io/costax/examplesapi/ExamplesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.costax.examplesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
