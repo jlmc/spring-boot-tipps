bc2ee5411e6d706863c5591f0960f7369f7cabf9
Joao Costa <costajlmpp@gmail.com>
2019-12-08 22:37:55 +0000

    3-Introduction to JPA and Hibernate

---
+++ introduction-to-jpa-and-hibernate/src/main/java/io/costax/introductiontojpaandhibernate/IntroductionToJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package io.costax.introductiontojpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ introduction-to-jpa-and-hibernate/src/test/java/io/costax/introductiontojpaandhibernate/IntroductionToJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.costax.introductiontojpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
