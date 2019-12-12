a5ef175208fc4931be10d78d4b8ae0b4579edf8d
Joao Costa <costajlmpp@gmail.com>
2019-12-12 22:59:10 +0000

    Project Api example

---
+++ food4-u/src/main/java/io/costax/food4u/Food4UApplication.java
@@ -0,0 +1,13 @@
+package io.costax.food4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Food4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Food4UApplication.class, args);
+	}
+
+}
---
+++ food4-u/src/test/java/io/costax/food4u/Food4UApplicationTests.java
@@ -0,0 +1,13 @@
+package io.costax.food4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Food4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
