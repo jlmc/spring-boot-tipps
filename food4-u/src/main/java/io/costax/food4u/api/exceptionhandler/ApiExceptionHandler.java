01949855764789b21ce3611f47a1dbb3fc993c09
Joao Costa <costajlmpp@gmail.com>
2019-12-21 20:00:37 +0000

    8.21. Handling exception InvalidFormatException in deserialization

--- food4-u/src/main/java/io/costax/food4u/api/exceptionhandler/ApiExceptionHandler.java
+++ food4-u/src/main/java/io/costax/food4u/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,8 +1,11 @@
 package io.costax.food4u.api.exceptionhandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import io.costax.food4u.domain.exceptions.BusinessException;
 import io.costax.food4u.domain.exceptions.ResourceInUseException;
 import io.costax.food4u.domain.exceptions.ResourceNotFoundException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Clock;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -28,11 +32,35 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   final HttpHeaders headers,
                                                                   final HttpStatus status,
                                                                   final WebRequest request) {
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException) {
+            return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+        }
+
         ProblemType problemType = ProblemType.MESSAGE_NOT_READABLE;
         String detail = "The request body is invalid. Check syntax error. " + ex.getMessage();
+        Problem problem = Problem.createBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleInvalidFormatException(final InvalidFormatException ex,
+                                                                final HttpHeaders headers,
+                                                                final HttpStatus status,
+                                                                final WebRequest request) {
+        String path = ex.getPath().stream()
+                .map(JsonMappingException.Reference::getFieldName)
+                .collect(Collectors.joining("."));
+
+        ProblemType problemType = ProblemType.MESSAGE_NOT_READABLE;
+
+        String detail = String.format(
+                """
+                The property '%s' contain the value '%s', witch is of invalid type. a value of the type '%s' should be used.
+                """, path, ex.getValue(), ex.getTargetType().getSimpleName());
 
         Problem problem = Problem.createBuilder(status, problemType, detail).build();
-
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
 
