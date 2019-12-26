b21f13fad612c334b91699266c25075fb0f5ae82
Joao Costa <costajlmpp@gmail.com>
2019-12-26 15:29:30 +0000

    11.7. Configuring and refactoring the project to use UTC

--- food4-u/src/main/java/io/costax/food4u/Food4UApplication.java
+++ food4-u/src/main/java/io/costax/food4u/Food4UApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 /**
  * By default we don't need to use the @EnableJpaRepositories annotation,
  * because the JpaRepositories are already enable by default.
@@ -17,6 +20,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Food4UApplication {
 
     public static void main(String[] args) {
+        //We must be careful when we use the method 'TimeZone.setDefault ()' it affects all applications running on the same JVM
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC").getId()));
         SpringApplication.run(Food4UApplication.class, args);
     }
 
