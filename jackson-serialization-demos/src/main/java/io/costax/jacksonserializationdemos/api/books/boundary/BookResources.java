18bfb0b9fee4eedb1950021a157a7785d5119cb6
Joao Costa <costajlmpp@gmail.com>
2019-12-31 00:29:27 +0000

    13.3. Using Jackson annotations JsonView and JsonFilter

---
+++ jackson-serialization-demos/src/main/java/io/costax/jacksonserializationdemos/api/books/boundary/BookResources.java
@@ -0,0 +1,74 @@
+package io.costax.jacksonserializationdemos.api.books.boundary;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import io.costax.jacksonserializationdemos.api.books.control.Books;
+import io.costax.jacksonserializationdemos.api.books.entity.Book;
+import io.costax.jacksonserializationdemos.api.books.jackson.Views;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookResources {
+
+    @Autowired
+    Books books;
+
+    /**
+     * http://localhost:8080/books
+     */
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Book> getAll() {
+        return books.list();
+    }
+
+    @JsonView(Views.BookNames.class)
+    @GetMapping(params = "projection=only-name")
+    public List<Book> listOnlyNames() {
+        return getAll();
+    }
+
+    @JsonView(Views.BookSummary.class)
+    @GetMapping(params = "projection=summary")
+    public List<Book> listSummaries() {
+        return getAll();
+    }
+
+    /**
+     * This can produce 3 types of output
+     *
+     * - http://localhost:8080/books/projections
+     *
+     * - http://localhost:8080/books/projections?type=name
+     *
+     * - http://localhost:8080/books/projections?type=summary
+     */
+    @GetMapping(value = "/projections")
+    @ResponseStatus(HttpStatus.OK)
+    public MappingJacksonValue getProjection(@RequestParam(required = false) String type) {
+        final List<Book> all = getAll();
+
+        MappingJacksonValue restaurantsWrapper = new MappingJacksonValue(all);
+
+        if ("name".equals(type)) {
+            restaurantsWrapper.setSerializationView(Views.BookNames.class);
+        } else if ("summary".equals(type)) {
+            restaurantsWrapper.setSerializationView(Views.BookSummary.class);
+        }
+
+        return restaurantsWrapper;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> getById(@PathVariable("id") Integer id) {
+        return books.book(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+}
---
+++ jackson-serialization-demos/src/main/java/io/costax/jacksonserializationdemos/api/books/jackson/Views.java
@@ -0,0 +1,12 @@
+package io.costax.jacksonserializationdemos.api.books.jackson;
+
+public final class Views {
+
+    private Views() {
+        throw new AssertionError();
+    }
+
+    public interface BookNames {}
+
+    public interface BookSummary {}
+}
