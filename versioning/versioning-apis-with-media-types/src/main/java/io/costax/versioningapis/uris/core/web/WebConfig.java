b32c9e0a7f2df12c026812f87a6ccbe33a0ab9f3
Joao Costa <costajlmpp@gmail.com>
2020-02-11 00:58:05 +0000

    20.20. Deprecating and Shutdown an API version resist interceptors

--- versioning/versioning-apis-with-media-types/src/main/java/io/costax/versioningapis/uris/core/web/WebConfig.java
+++ versioning/versioning-apis-with-media-types/src/main/java/io/costax/versioningapis/uris/core/web/WebConfig.java
@@ -1,12 +1,17 @@
 package io.costax.versioningapis.uris.core.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    ApiDeprecationHandler apiDeprecationHandler;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -26,5 +31,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(ApiExampleMediaTypes.V2_APPLICATION_JSON);
+    }
+
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        // if api v1 steel valid but deprecated
+        registry.addInterceptor(apiDeprecationHandler);
     }
 }
--- versioning/versioning-apis-with-uris/src/main/java/io/costax/versioningapis/uris/core/web/WebConfig.java
+++ versioning/versioning-apis-with-uris/src/main/java/io/costax/versioningapis/uris/core/web/WebConfig.java
@@ -1,11 +1,18 @@
 package io.costax.versioningapis.uris.core.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    ApiDeprecationHandler apiDeprecationHandler;
+
+    ApiRetirementHandler apiRetirementHandler;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -13,6 +20,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*");
         //.allowedOrigins("*")
         //.maxAge(30);
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        // if api v1 steel valid but deprecated
+        registry.addInterceptor(apiDeprecationHandler);
+
+        // if the api v1 is not valid any more
+        //registry.addInterceptor(apiRetirementHandler);
     }
 
 }
