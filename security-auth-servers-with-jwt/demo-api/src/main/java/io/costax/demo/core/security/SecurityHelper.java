6f6cfa43687275081cce456cf248f40232de913c
Joao Costa <costajlmpp@gmail.com>
2020-02-29 23:38:43 +0000

    23.17. Obter user autenticado no Resource Server

---
+++ security-auth-servers-with-jwt/demo-api/src/main/java/io/costax/demo/core/security/SecurityHelper.java
@@ -0,0 +1,34 @@
+package io.costax.demo.core.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+@Component
+public class SecurityHelper {
+
+    private SecurityContext getContext() {
+        return SecurityContextHolder
+                .getContext();
+    }
+
+    /**
+     * The Authentication represent the authenticated Token
+     */
+    public Authentication getAuthentication() {
+
+        return getContext()
+                .getAuthentication();
+    }
+
+    public Integer getUserId() {
+        final Authentication authentication = getAuthentication();
+
+        final Jwt jwt = (Jwt) authentication.getPrincipal();
+        final Integer userId = jwt.getClaim("user_id");
+
+        return userId;
+    }
+}
