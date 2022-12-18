8ae44fddc04ef1d9f9d999cb63940c79c8a1aae7
Joao Costa <costajlmpp@gmail.com>
2022-12-18 17:16:47 +0000

    korders-processor-consumers-error-handler-initial-project

---
+++ spring-boot-tipps-kafka/korders-processor-consumers-error-handler/src/main/java/io/github/jlmc/korders/processor/KOrdersProcessorConsumersApplication.java
@@ -0,0 +1,13 @@
+package io.github.jlmc.korders.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KOrdersProcessorConsumersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KOrdersProcessorConsumersApplication.class, args);
+    }
+
+}
