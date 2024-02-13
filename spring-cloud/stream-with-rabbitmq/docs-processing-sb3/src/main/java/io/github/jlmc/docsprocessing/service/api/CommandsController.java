61e130f88fd1d9a29b7c2b5a40c2930cbd9f5fc1
Joao Costa <costajlmpp@gmail.com>
2024-02-13 19:23:51 +0000

    Bump to spring boot 3.2.2

--- spring-cloud/stream-with-rabbitmq/docs-processing-sb3/src/main/java/io/github/jlmc/docsprocessing/service/api/CommandsController.java
+++ spring-cloud/stream-with-rabbitmq/docs-processing-sb3/src/main/java/io/github/jlmc/docsprocessing/service/api/CommandsController.java
@@ -1,7 +1,6 @@
 package io.github.jlmc.docsprocessing.service.api;
 
 import io.github.jlmc.docsprocessing.service.infrastructure.bpm.messaging.producer.DefaultBpmProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
