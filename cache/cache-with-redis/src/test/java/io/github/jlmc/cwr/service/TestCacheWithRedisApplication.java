7cee9cf5d5eb39e950bc31d284f887b0da5771ac
Joao Costa <costajlmpp@gmail.com>
2024-03-10 23:10:10 +0000

    Application context test container

--- cache/cache-with-redis/src/test/java/io/github/jlmc/cwr/service/TestCacheWithRedisApplication.java
+++ cache/cache-with-redis/src/test/java/io/github/jlmc/cwr/service/TestCacheWithRedisApplication.java
@@ -1,5 +1,6 @@
 package io.github.jlmc.cwr.service;
 
+import org.junit.Ignore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+@Ignore
 @TestConfiguration(proxyBeanMethods = false)
 public class TestCacheWithRedisApplication {
 
